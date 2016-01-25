d26222421b5979b6bd65d13b0fbae43a97796078
Justin Rawson <Tinister@users.noreply.github.com>
2016-01-24 23:30:58 -0800

    Created initial projects.

---
+++ IntervalTree/IntervalTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntervalTreeNS
+{
+	/// <summary>
+	/// Represents a strongly-typed collection of objects that can be represented by intervals.
+	/// </summary>
+	/// <typeparam name="TElement">The type of elements in the tree.</typeparam>
+	/// <typeparam name="TEndpoint">The type of the endpoints of the interval each element represents.</typeparam>
+	public class IntervalTree<TElement, TEndpoint>
+	{
+	}
+}
