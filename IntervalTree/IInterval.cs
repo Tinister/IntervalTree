9d91b31f9843abfc6f8305a09776a3851135da23
Justin Rawson <Tinister@users.noreply.github.com>
2016-01-26 21:14:34 -0800

    Added basic interval types.

---
+++ IntervalTree/IInterval.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IntervalTreeNS
+{
+	/// <summary>Represents an interval with the specified endpoint type.</summary>
+	/// <typeparam name="TEndpoint">Type of the interval endpoints.</typeparam>
+	public interface IInterval<out TEndpoint>
+		where TEndpoint : IComparable<TEndpoint>
+	{
+		/// <summary>Gets the inclusive starting point of the interval.</summary>
+		TEndpoint Start { get; }
+
+		/// <summary>Gets the inclusive ending point of the interval.</summary>
+		[SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "End",
+			Justification = "Not sure which .NET languages reserves 'End'.")]
+		TEndpoint End { get; }
+	}
+}
---
+++ IntervalTree/Interval.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntervalTreeNS
+{
+	/// <summary>A simple implementation of the <see cref="IInterval{TEndpoint}"/> interface.</summary>
+	/// <typeparam name="TEndpoint">Type of the interval endpoints.</typeparam>
+	public struct Interval<TEndpoint> : IInterval<TEndpoint>
+		where TEndpoint : IComparable<TEndpoint>
+	{
+		/// <summary>Initializes a new instance of the <see cref="Interval{TEndpoint}"/> struct with the specified endpoints.</summary>
+		/// <param name="start">The inclusive starting point of the interval.</param>
+		/// <param name="end">The inclusive ending point of the interval.</param>
+		public Interval(TEndpoint start, TEndpoint end)
+		{
+			if (Comparer<TEndpoint>.Default.Compare(start, end) < 0)
+				throw new ArgumentException("Interval end must occur after the interval start.");
+
+			Start = start;
+			End = end;
+		}
+
+		/// <summary>Initializes a new instance of the <see cref="Interval{TEndpoint}"/> struct as a copy of the specified
+		/// interval.</summary>
+		/// <param name="interval">Interval to copy.</param>
+		public Interval(IInterval<TEndpoint> interval)
+		{
+			if (interval == null)
+				throw new ArgumentNullException(nameof(interval));
+			if (Comparer<TEndpoint>.Default.Compare(interval.Start, interval.End) < 0)
+				throw new ArgumentException("Interval end must occur after the interval start.");
+
+			Start = interval.Start;
+			End = interval.End;
+		}
+
+		/// <summary>Gets the inclusive starting point of the interval.</summary>
+		public TEndpoint Start { get; }
+
+		/// <summary>Gets the inclusive ending point of the interval.</summary>
+		public TEndpoint End { get; }
+	}
+}
